78056edd1016b966645dfce8cf7df56f13ae7710
Helen Goatly <hgoatly@hotmail.com>
2022-02-16 10:06:16 +0000

    Install Bootstrap and react-bootstrap, and add bootstrap navbar.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Component } from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
@@ -5,6 +6,8 @@ import { Route, Switch } from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   constructor() {
@@ -16,24 +19,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <header className="App-header">
+      
       <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         { this.state.string }
-        </p>
-        <button onClick={() => this.setState({ string: 'Hello Ugglis' })}>Change Greeting</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header>
-      <HomePage/>
+      <HomePage />
     </div>
     )
   }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 
 const Header = () => (
-  <div className='header'>
-    
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
-      </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-      </Link>
-     
-    </div>
-  </div>
+  
+  <Navbar bg="light" expand="lg">
+  <Container >
+    <Navbar.Brand href="#home">Tom &amp; Claire's Wedding</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Invitation</Nav.Link>
+        <NavDropdown title="Information" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Venues</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Places to Stay</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Gift List</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Additional Information</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
 );
 
 

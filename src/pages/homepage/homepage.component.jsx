a085e9f0b5cedb1ef4fbf02c32fc86cfc199aeb8
Helen Goatly <hgoatly@hotmail.com>
2022-02-20 15:40:56 +0000

    Hide scrollbar on x axis. Change hero image.

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 
 import './homepage.styles.css';
 
-import image from '../../img/tom-and-claire.jpg'
+import image from '../../img/sunset-kiss.jpg'
 
 
 function HomePage() {
     return (
-        <div style={{height: '100vh',
-                    width: '100vw'
+        <div 
+            style={{height: '100vh',
+                    width: '100vw',
                     }}>
         <div style={{ backgroundImage:`url(${image})`,
                       height:'100vh',
                       width: '100vw',
                       backgroundPosition: 'center',
                       backgroundSize: 'cover',
-                      backgroundRepeat: 'no-repeat'
+                      backgroundRepeat: 'no-repeat',
                     }}>
                     <div id="greeting">Tom &amp; Claire's Wedding</div>
         </div>
